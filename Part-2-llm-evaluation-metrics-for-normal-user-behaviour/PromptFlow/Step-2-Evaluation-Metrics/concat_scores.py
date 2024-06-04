f3532a6e2038b4c47bc6102a6cc81b00fb4f3672
Luca Stamatescu <developer@lucastamatescu.com>
2024-06-04 13:40:17 +1000

    add reasons to outputs. correct gpt_retrieval

--- Part-2-llm-evaluation-metrics-for-normal-user-behaviour/PromptFlow/Step-2-Evaluation-Metrics/concat_scores.py
+++ Part-2-llm-evaluation-metrics-for-normal-user-behaviour/PromptFlow/Step-2-Evaluation-Metrics/concat_scores.py
@@ -1,45 +1,52 @@
+import json
 from promptflow import tool
 import numpy as np
 import re
 
 
 @tool
-def concat_results(gpt_coherence_score: str = None,
-                   gpt_similarity_score: str = None,
-                   gpt_fluency_score: str = None,
-                   gpt_relevance_score: str = None,
-                   gpt_groundedness_score: str = None,
-                   f1_score: float = None,
-                   gpt_retrieval: str = None,
-                   analytical_retrieval: float = None,
-                   analytical_tool_selection: float = None,
-                   ada_cosine_similarity: float = None):
+def concat_results(gpt_coherence_output: str = None,
+                   gpt_similarity_output: str = None,
+                   gpt_fluency_output: str = None,
+                   gpt_relevance_output: str = None,
+                   gpt_groundedness_output: str = None,
+                   f1_output: float = None,
+                   gpt_retrieval_output: str = None,
+                   analytical_retrieval_output: float = None,
+                   analytical_tool_selection_output: float = None,
+                   ada_cosine_similarity_output: float = None):
 
-    load_list = [{'name': 'gpt_coherence', 'score': gpt_coherence_score},
-                 {'name': 'gpt_similarity', 'score': gpt_similarity_score},
-                 {'name': 'gpt_fluency', 'score': gpt_fluency_score},
-                 {'name': 'gpt_relevance', 'score': gpt_relevance_score},
-                 {'name': 'gpt_groundedness', 'score': gpt_groundedness_score},
-                 {'name': 'f1_score', 'score': f1_score},
-                 {'name': 'gpt_retrieval', 'score': gpt_retrieval},
-                 {'name': 'analytical_retrieval', 'score': analytical_retrieval},
-                 {'name': 'analytical_tool_selection', 'score': analytical_tool_selection},
-                 {'name': 'ada_similarity', 'score': ada_cosine_similarity}]
+    load_list = [{'name': 'gpt_coherence', 'output': gpt_coherence_output},
+                 {'name': 'gpt_similarity', 'output': gpt_similarity_output},
+                 {'name': 'gpt_fluency', 'output': gpt_fluency_output},
+                 {'name': 'gpt_relevance', 'output': gpt_relevance_output},
+                 {'name': 'gpt_groundedness', 'output': gpt_groundedness_output},
+                 {'name': 'f1_score', 'output': f1_output},
+                 {'name': 'gpt_retrieval', 'output': gpt_retrieval_output},
+                 {'name': 'analytical_retrieval', 'output': analytical_retrieval_output},
+                 {'name': 'analytical_tool_selection', 'output': analytical_tool_selection_output},
+                 {'name': 'ada_similarity', 'output': ada_cosine_similarity_output}]
 
     scalar_metrics = ["f1_score", "ada_similarity","analytical_retrieval","analytical_tool_selection"]
     score_list = []
     errors = []
     for item in load_list:
-        if item['score']:
+        if item['output']:
             try:
-                score = item["score"]
+                output = item["output"]
                 if item["name"] not in scalar_metrics:
-                    match = re.search(r'\d', score)
-                    if match:
-                        score = match.group()
-                    score = float(score)
+                    try:
+                        # First try reading it as a JSON object, and extract the star rating
+                        score=json.loads(output)["stars"]
+                        score = float(score)
+                    except:
+                        # If that fails, try reading it as a string and extract the star rating
+                        match = re.search(r'\d', output)
+                        if match:
+                            score = match.group()
+                        score = float(score)
                 else:
-                    score = round(float(score),3)
+                    score = round(float(output),3)
             except Exception as e:
                 score = np.nan
                 errors.append({"name": item["name"], "msg":   str(e), "data": item["score"]})
